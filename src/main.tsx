7d1f39693ee23343b57431213d9ec108fa714cff
Freddy <161765245+FotsFreddy@users.noreply.github.com>
2025-02-13 19:07:38 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ createRoot(document.getElementById('root')!).render(
     <Routes>
       <Route path='/' element={<App />} />
     </Routes>
-      {<App />}
     </BrowserRouter>
   </StrictMode>,
 )
