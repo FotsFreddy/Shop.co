7d54b91d4575d4c6c3db55f5b6f90d48bbf79751
Freddy <fotsofreddy26gamil.com>
2025-02-13 22:39:55 +0100

    added the home page route

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
 import App from './App'
+import HomePage from './pages/homePage/HomePage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
+      <Route path='/homepage' element={<HomePage />} />
     </Routes>
     </BrowserRouter>
   </StrictMode>,
