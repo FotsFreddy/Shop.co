e54c8aef7573e462bc5ae66c7d734db656353626
Freddy <fotsofreddy26gamil.com>
2025-02-15 16:29:33 +0100

    enabled mobile search for products added banner closing button

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router";
-import { Menu, Search, ShoppingCart } from "lucide-react";
+import { Menu, Search, ShoppingCart, X } from "lucide-react";
 import Logo from "../logo/Logo";
 import Input from "../input/Input";
 
@@ -14,13 +14,15 @@ interface NavItemProps {
 export default function Header() {
   const [dropDown, setDropDown] = useState(false);
   return (
-    <header>
+    <header className={`sticky top-0 z-50 bg-white shadow-md`}>
       <Banner />
       <div className="flex justify-between items-center p-2 md:justify-center md:gap-12">
         <div className="flex gap-3 items-center justify-center text-nowrap sm:gap-3 md:gap-5">
           <Menu
-            className="z-50 cursor-pointer sm:w-9 sm:h-w-9 md:hidden"
+            className="z-50 cursor-pointer active:scale-90 sm:w-9 sm:h-w-9 md:hidden"
             onClick={() => setDropDown(!dropDown)}
+            aria-label="Menu"
+            tabIndex={0}
           />
           <Logo />
           <MobileNavBar dropDown={dropDown} />
@@ -28,20 +30,32 @@ export default function Header() {
         </div>
         <div className="flex items-center justify-center gap-3 sm:w-2/5">
           <SearchForm />
-          <ShoppingCart className="cursor-pointer" />
+          <Link to={"/Category"}>
+            <ShoppingCart
+              className="cursor-pointer max-sm:w-8 active:scale-90 hover:text-gray-600 transition"
+              aria-label="Shopping Cart"
+            />
+          </Link>
         </div>
       </div>
     </header>
   );
 }
 function Banner() {
+  const [showBanner, setShowBanner] = useState(true);
   return (
-    <p className="bg-black text-white text-center text-sm p-1 sm:text-lg">
-      Sign up and get 20% off to your first order.{" "}
-      <Link to={"/"} className="underline underline-offset-2">
-        Sign Up Now
-      </Link>
-    </p>
+    <div className={`bg-black text-white p-2 transition ${showBanner ? "block" : "hidden max-sm:block"}`}>
+      <p className="text-center text-sm sm:text-lg">
+        Sign up and get 20% off to your first order.{" "}
+        <Link to={"/"} className="underline underline-offset-2">
+          Sign Up Now
+        </Link>
+      </p>
+      <X className="hidden sm:block cursor-pointer text-white absolute md:right-[14%] top-3 active:scale-90 sm:right-2"
+        onClick={() => setShowBanner(!showBanner)}
+        aria-label="Close Banner" 
+      />
+    </div>
   );
 }
 function NavBar() {
@@ -99,16 +113,27 @@ function Navitem({ NavItem }: NavItemProps) {
   );
 }
 function SearchForm() {
+  const [isVisible, setIsVisible] = useState(false); {/* invisible on mobile */}
   return (
-    <form action=""method="get" className={`sm:w-full`}>
+    <form action="" method="get" className="sm:w-full">
       <div className="relative">
+        {/* search icon */}
         <Search
-          className={`cursor-pointer left-2 top-3 sm:absolute sm:text-gray-400 md:pointer-events-none`}
+          className={`cursor-pointer left-2 top-3 
+            sm:absolute sm:text-gray-400 sm:pointer-events-none md:block
+            hover:text-gray-600 transition ${isVisible?"hidden":""}`}
+          onClick={() => setIsVisible(true)}
+          aria-label="Search"
         />
         <Input
           inputType="search"
           inputPlaceholder="Search for products..."
-          inputStyle={`hidden w-full bg-[#f0f0f0] rounded-full px-9 py-3 w-full sm:block`}
+          inputStyle={`w-full bg-[#f0f0f0] rounded-full sm:px-9 py-3 sm:block ${isVisible ? "pl-3 pr-8" : "hidden" }`}
+        />
+        {/* close icon */}
+        <X className={`cursor-pointer absolute right-2 top-3 text-gray-400 w-5 ${isVisible?"block":"hidden"}`}
+          onClick={() => setIsVisible(false)} 
+          aria-label="Close Search Bar" 
         />
       </div>
     </form>
