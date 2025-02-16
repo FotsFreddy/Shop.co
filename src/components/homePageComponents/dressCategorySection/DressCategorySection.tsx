4d597ae988e778b3e41b40ad90519e5e24013517
Freddy <fotsofreddy26gamil.com>
2025-02-16 15:46:39 +0100

    turned into a section tag and increased height from md screens

--- src/components/homePageComponents/dressCategorySection/DressCategorySection.tsx
+++ src/components/homePageComponents/dressCategorySection/DressCategorySection.tsx
@@ -25,22 +25,22 @@ const dressStyle:Array<{imageSrc:string, title:string}> = [
 export default function DressCategorySection() {
     const tab = new Set([1, 2]);
   return (
-    <div className="bg-[#F0F0F0] rounded-3xl px-4 py-4 mx-4 my-10 md:p-10 md:mt-14 md:mx-12">
+    <section className="bg-[#F0F0F0] rounded-3xl px-4 py-4 mx-4 my-10 md:p-10 md:mt-14 md:mx-12">
       <h2 className="text-4xl text-center font-[1000] pb-4 max-sm:w-4/5 m-auto md:pb-9">BROWSE BY DRESS STYLE</h2>
       <div className="grid grid-flow-row gap-4 md:grid-cols-3 ">
         {dressStyle.map((item, index) => (
           <DressStyle item={item} key={index} colspan={tab.has(index)} />
         ))}
       </div>
-    </div>
+    </section>
   );
 }
 
 function DressStyle({item, colspan}:DressStyleProps) {
     return (
-       <div className={`relative rounded-3xl overflow-hidden h-52 cursor-pointer md:h-64 ${colspan ? "md:col-span-2" : ""}`}>
-        <Link to={"/Category"} aria-label={`Browse ${item.title} style`}>
-            <img src={item.imageSrc} alt={item.title} className="w-full h-full  transition-all duration-300 object-cover hover:scale-110"/>
+       <div className={`relative rounded-3xl overflow-hidden h-52 cursor-pointer md:h-72 ${colspan ? "md:col-span-2" : ""}`}>
+        <Link to={"/category"} aria-label={`Browse ${item.title} style`}>
+            <img src={item.imageSrc} alt={item.title} className="w-full h-full  transition-all duration-300 object-center hover:scale-110"/>
             <h4 className="absolute top-5 left-5 font-semibold md:text-2xl md:font-bold">{item.title}</h4>
         </Link>
        </div>
