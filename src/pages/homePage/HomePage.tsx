709cdf138faa807ddd4adff9a4201d76d37c3d94
Freddy <fotsofreddy26gamil.com>
2025-02-16 12:09:49 +0100

    Home page fully done

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,4 +1,6 @@
+import BrandBanner from "../../components/homePageComponents/brandBanner/BrandBanner";
 import DressCategorySection from "../../components/homePageComponents/dressCategorySection/DressCategorySection";
+import HeroSection from "../../components/homePageComponents/heroSection/HeroSection";
 import ProductSection from "../../components/homePageComponents/productSection/ProductSection";
 import ReviewSection from "../../components/homePageComponents/reviewSection/ReviewSection";
 import Footer from "../../components/ui/footer/Footer";
@@ -8,6 +10,8 @@ export default function HomePage() {
   return (
     <>
       <Header />
+      <HeroSection />
+      <BrandBanner />
       <ProductSection sectionTitle={"NEW ARRIVALS"} />
       <hr className="border-gray-300 mx-4 my-10 md:my-14 md:mx-12 "/>
       <ProductSection sectionTitle={"TOP SELLING"} />
